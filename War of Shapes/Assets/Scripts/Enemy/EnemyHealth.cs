148d401dcfbf1eb9f15bf9fb9902d3a422b7da39
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-04-14 17:10:11 +0530

    Player and Enemy Health

---
+++ War of Shapes/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [Header("Health Settings")]
+    [SerializeField] private int maxHealth = 50;
+    [SerializeField] private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if(currentHealth < 0)
+        {
+            currentHealth = 0;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ War of Shapes/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Header("Health Settings")]
+    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if(currentHealth < 0)
+        {
+            currentHealth = 0;
+            Destroy(gameObject);
+        }
+    }
+}
