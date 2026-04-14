4428c3c42cb3b80e81858e40f75fb992b222b6ae
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-04-14 10:14:36 +0530

    Assets for the Game

---
+++ War of Shapes/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 5f;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Inputs")]
+    [SerializeField] private float moveInputX;
+    [SerializeField] private float moveInputY;
+
+    private void Update()
+    {
+        moveInputX = Input.GetAxisRaw("Horizontal");
+        moveInputY = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+
+    }
+}
