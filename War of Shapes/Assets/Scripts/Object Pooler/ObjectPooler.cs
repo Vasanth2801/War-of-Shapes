9a9d63d1ce5f982c597f1bacccbd94ddec0c0da0
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-04-14 14:50:51 +0530

    Object Pooling

---
+++ War of Shapes/Assets/Scripts/Object Pooler/ObjectPooler.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [System.Serializable]
+    public class ObjectPoolItem
+    {
+        public string objectTag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public ObjectPoolItem[] pools;
+    public Dictionary<string, Queue<GameObject>> poolsOfDictionary;
+
+    private void Start()
+    {
+        poolsOfDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach(ObjectPoolItem pool in pools)
+        {
+            Queue<GameObject> obj = new Queue<GameObject>();
+
+            for(int i =0; i< pool.size; i++)
+            {
+                GameObject objPool = Instantiate(pool.prefab);
+                objPool.SetActive(false);
+                obj.Enqueue(objPool);
+            }
+
+            poolsOfDictionary.Add(pool.objectTag, obj);
+        }
+    }
+
+    public GameObject SpawnFromPools(string tag, Vector3 position, Quaternion rotation)
+    {
+        GameObject objToSpawn = poolsOfDictionary[tag].Dequeue();
+        objToSpawn.SetActive(true);
+        objToSpawn.transform.position = position;
+        objToSpawn.transform.rotation = rotation;
+
+        poolsOfDictionary[tag].Enqueue(objToSpawn);
+
+        return objToSpawn;
+    }
+}
